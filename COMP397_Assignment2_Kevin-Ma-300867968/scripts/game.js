e9d96b06f5acfe822a4392459b4407cdcc785630
kb-ma <kbma.kevin@gmail.com>
2016-06-12 00:06:27 -0400

    v0.3 - added game instructions, first few stages are functioning properly

--- COMP397_Assignment2_Kevin-Ma-300867968/scripts/game.js
+++ COMP397_Assignment2_Kevin-Ma-300867968/scripts/game.js
@@ -6,8 +6,8 @@
  *               the functionalities of the web game.
  *
  *  Date Created: June 11, 2016
- *  Date Last Modified: June 11, 2016
- *  Version: 0.2 - added internal documentation
+ *  Date Last Modified: June 12, 2016
+ *  Version: 0.3 - added game instructions, first few stages are functioning properly
  */
 /// <reference path="typings/impress/impress.d.ts" />
 /**
@@ -21,8 +21,8 @@
     console.log("Game started...");
     impress().init();
     // Game Variables ++++++++++++++++++++++++++++++++++++++
-    var strength = 10;
-    var hoursRemaining = 10;
+    var strength;
+    var hoursRemaining;
     var gameOver = false;
     // Event Listeners +++++++++++++++++++++++++++++++++++++
     $('a').on('click', function () {
@@ -33,11 +33,20 @@
         //    strength = 10;
         //    hoursRemaining = 10;
         //}
-        if (destination == '2') {
-            strength += 100;
-            hoursRemaining -= 3;
-            console.log(location.href.substring(0, location.href.length - 2) + '4');
+        switch (destination) {
+            case "1":
+                strength = 10;
+                hoursRemaining = 10;
+                break;
+            case "2":
+                strength += 100;
+                break;
         }
+        //if (destination == '2') {
+        //    strength += 100;
+        //    hoursRemaining -= 3;
+        //    console.log(location.href.substring(0, location.href.length - 2) + '4');
+        //}
         //if (hoursRemaining <= 0) {
         //    gameOver = true;
         //}
--- COMP397_Assignment2_Kevin-Ma-300867968/scripts/game.ts
+++ COMP397_Assignment2_Kevin-Ma-300867968/scripts/game.ts
@@ -6,8 +6,8 @@
  *               the functionalities of the web game.
  *
  *  Date Created: June 11, 2016
- *  Date Last Modified: June 11, 2016
- *  Version: 0.2 - added internal documentation
+ *  Date Last Modified: June 12, 2016
+ *  Version: 0.3 - added game instructions, first few stages are functioning properly
  */
 
 /// <reference path="typings/impress/impress.d.ts" />
@@ -24,8 +24,8 @@
     impress().init();
 
     // Game Variables ++++++++++++++++++++++++++++++++++++++
-    var strength: number = 10;
-    var hoursRemaining: number = 10;
+    var strength: number;
+    var hoursRemaining: number;
     var gameOver: boolean = false;
 
     // Event Listeners +++++++++++++++++++++++++++++++++++++
@@ -39,11 +39,22 @@
         //    strength = 10;
         //    hoursRemaining = 10;
         //}
-        if (destination == '2') {
-            strength += 100;
-            hoursRemaining -= 3;
-            console.log(location.href.substring(0, location.href.length - 2) + '4');
+        switch (destination) {
+
+            case "1":
+                strength = 10;
+                hoursRemaining = 10;
+                break;
+            case "2":
+                strength += 100;
+                break;
         }
+
+        //if (destination == '2') {
+        //    strength += 100;
+        //    hoursRemaining -= 3;
+        //    console.log(location.href.substring(0, location.href.length - 2) + '4');
+        //}
 
         //if (hoursRemaining <= 0) {
         //    gameOver = true;
