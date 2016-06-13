6e071bb124a9b7380046045567c0e5f6d75fa4d2
kb-ma <kbma.kevin@gmail.com>
2016-06-13 00:16:41 -0400

    v1.0 - added all remaining events to display, initial release

--- COMP397_Assignment2_Kevin-Ma-300867968/scripts/game.js
+++ COMP397_Assignment2_Kevin-Ma-300867968/scripts/game.js
@@ -6,8 +6,8 @@
  *               the functionalities of the web game, Game of Survival.
  *
  *  Date Created: June 11, 2016
- *  Date Last Modified: June 12, 2016
- *  Version:  0.10 - connected all paths flow from beginning to end
+ *  Date Last Modified: June 13, 2016
+ *  Version:  1.0 - added all remaining events to display, initial release
  */
 /// <reference path="typings/impress/impress.d.ts" />
 /**
@@ -21,7 +21,6 @@
     var strength;
     var enemyStrength;
     var hoursRemaining;
-    var gameOver = false;
     var evtLog;
     // INITIALIZATION ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     console.log("Game started...");
@@ -31,7 +30,7 @@
     $('#playerStats').hide();
     // NAMED FUNCTIONS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     /**
-     * This function sets assigns a string argument to the evtLog variable which
+     * This function assigns a string argument to the evtLog variable which
      * will be displayed in the evt-log-body in the index.html file.
      *
      * @method evtLogMsg
@@ -39,22 +38,23 @@
      * @returns {void}
      */
     function evtLogMsg(msg) {
-        evtLog = msg + '<br />------<br /><br />';
+        evtLog = msg + '<br /><br />' + "You have " + hoursRemaining +
+            " hour(s) left before nuclear warheads are going to hit the island."
+            + '<br /><br />------<br /><br />';
     }
     // EVENT LISTENERS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // This event listener will trigger each time the player makes a choice
     $('a').on('click', function () {
         // assign the name of the div that the anchor will go to, to this var
         var destination = this.href.substring(this.href.indexOf('#') + 1);
         evtLog = ""; // clear contents everytime enter new stage
-        console.log(this.href);
-        console.log("destination: " + destination);
         /**
-            This switch serves as a trigger board. It handles
+            This switch construct serves as a trigger board. It handles
             different events which occur during the storyline
             of the game.
         */
         switch (destination) {
-            // When player starts a [new] game
+            // When player starts a [new] game hide game panels
             case "game":
                 $('#enemyStats').hide();
                 $('#events-log').hide();
@@ -62,61 +62,113 @@
                 break;
             // The story begins at div id #stage1, need to initialize variables
             case "stage1":
-                $('#evt-log-body').html('');
-                $('#events-log').show();
+                $('#evt-log-body').html(''); // clear contents of event log
+                $('#events-log').show(); // display other game panels when game starts
                 $('#playerStats').show();
-                strength = 90;
-                enemyStrength = 10;
-                hoursRemaining = 10;
+                strength = 90; // player starts with 90 strength
+                enemyStrength = 10; // enemy starts with 10 strength
+                hoursRemaining = 10; // 10 hours before game over
                 evtLogMsg('=================================' + '<br />' +
                     'GAME START!' + '<br />' +
                     '=================================' + '<br />' +
-                    'You wake up and do not know where you are...' + '<br /><br />' +
-                    "You have " + hoursRemaining + " hours left before nuclear warheads are going to hit the island.");
+                    'You wake up and do not know where you are...');
                 break;
+            // remain at forest of origin
             case "stage2.1-stage1-choice1":
-                hoursRemaining -= 3; // TEST
+                hoursRemaining -= 2; // you waited 2 hours
                 strength += 20; // from push-ups
                 enemyStrength += 100; // because you did not take sword, enemy did
-                //evtLog = "You developed your muscles from doing push-ups.<br />[+20 STR]<br /><br />" + evtLog;
-                //evtLog = "You have " + hoursRemaining + " hours left before nuclear warheads are going to hit the island."
-                //    + '<br /><br />' + evtLog;
-                //evtLog = "Test messsage. Moved to stage: " + destination + '<br />------<br />' + evtLog;
+                evtLogMsg('You observed from a distance.<br />[-2 hours remaining]<br />' +
+                    '<br /> You developed your muscles from doing push-ups.<br />[+20 STR]');
+                break;
+            // head towards mysterious light
+            case "stage2.2-stage1-choice2":
+                hoursRemaining -= 5;
+                evtLogMsg('You walk for a long time...<br />[-5 hours remaining]');
+                break;
+            // head towards Void of Darkness
+            case "stage3.1-stage2.1-choice1":
+                hoursRemaining -= 5;
+                evtLogMsg('You slowly find your way to the origin of the light.<br />[-5 hours remaining]');
                 break;
             // Display the enemy's statistics due to encounter
+            // escape to the island's heart
             case "stage3.2-stage2.1-choice2":
+                hoursRemaining -= 6;
+                $('#enemyStats').show();
+                evtLogMsg("Without any light, it takes you a long time to find the Island's Heart." +
+                    "<br />[-6 hours remaining]");
+            // run away with the sword to the island's heart
             case "stage3.3-stage2.2-choice1":
+                hoursRemaining -= 3;
+                strength += 100; // acquired Sword of Light
+                evtLogMsg("When you took the sword, the light vanished. Without any light, it takes you a" +
+                    " long time to find the Island's Heart.<br />[-3 hours remaining]");
+                $('#enemyStats').show();
+            // run away from the light, to the island's heart
             case "stage3.4-stage2.2-choice2":
+                hoursRemaining -= 3;
+                enemyStrength += 100; // because you didn't take the sword, enemy took it
+                evtLogMsg("As you leave the vicinity, the light suddenly vanishes. Without any light, it takes you a" +
+                    " long time to find the Island's Heart.<br />[-3 hours remaining]");
                 $('#enemyStats').show();
                 //evtLog = "Test messsage. Moved to stage: " + destination + '<br />------<br />' + evtLog;
                 break;
             // RESULTS -------------------------------------------------------------------------------
+            // Monster kills you
             case "result1-stage3.1-choice1":
                 $('#game-over-body').text('A demon suddenly leaps from within the sheath and devours your body!');
                 break;
+            // Too Slow
             case "result2-stage3.1-choice2":
+                hoursRemaining -= 3;
                 $('#game-over-body').text('You were too late. With no portal left, you watch in despair as the nuclear missles rain down from above.');
                 break;
+            // Stalemate fight
             case "result3-stage3.2-choice1":
+                hoursRemaining -= 2;
                 $('#game-over-body').text('As the fight dragged out, the time remaining disappeared. The both of you watch in despair as nuclear missles rain down from above.');
                 break;
+            // Backstabbed by enemy
             case "result4-stage3.2-choice2":
+                hoursRemaining -= 1;
                 $('#game-over-body').text('While you were deep in thought, the other person took a knife and stabbed you in the back. The knife plunged through your heart almost instantly.');
                 break;
+            // Kill the enemy
             case "result5-stage3.3-choice1":
+                hoursRemaining -= 0.33;
                 $('#win-game-body').text('And off comes his/her head! As a fountain of blood errupts from the severed body, you make a quick victory dance. You then enter the portal and safely make it back home.');
                 break;
+            // Run out of time thinking of solution
             case "result6-stage3.3-choice2":
+                hoursRemaining -= 2;
                 $('#game-over-body').text('The two of you bounce ideas back and forth in an attempt to find a suitable solution. Unfortunately, there was not enough time. Nuclear missles started plummeting down from above.');
                 break;
+            // slaughtered by enemy
             case "result7-stage3.4-choice1":
+                hoursRemaining -= 1;
                 $('#game-over-body').text('But you are completely outpowered. The enemy easily deflects all your attacks and with one swift slash, cuts you in half. Your organs plummet to the ground as your enemy disappears through the portal.');
                 break;
+            // beheaded by enemy while pleading for mercy
             case "result8-stage3.4-choice2":
+                hoursRemaining -= 0.5;
                 $('#game-over-body').text('While you are still talking you hear a snicker, before you feel a quick but sharp pain at the back of your neck.The next thing you know, your head rolls across the floor, and you see blood spewing from what looks like your body...');
                 break;
-            default:
-                evtLogMsg("Test messsage. Moved to stage: " + destination);
+            // GAME END -------------------------------------------------------------------------------
+            // Didn't use evtLogMsg here because don't want the hours remaining message to display here
+            case "game-over":
+                evtLog = '=================================' + '<br />' +
+                    'GAME OVER...' + '<br />' +
+                    '=================================' + '<br />' +
+                    'You have lived a valiant life but due to poor decisions made, you have died.' +
+                    '<br />Please try again!< br />------<br /><br />';
+                break;
+            case "win-game":
+                evtLog = '=================================' + '<br />' +
+                    'YOU WIN!' + '<br />' +
+                    '=================================' + '<br />' +
+                    'Through your determination and sheer willpower you have successfully made it out ' +
+                    'alive from the Game of Survival.<br />Do you dare to play again?< br />------<br /><br />';
                 break;
         }
         // updates the in-game stats whenever moving to new stage
@@ -124,5 +176,5 @@
         $('#str').text(strength.toString());
         $('.time').text(hoursRemaining.toString());
         $('#evt-log-body').html(evtLog + $('#evt-log-body').html());
-    });
-})();
+    }); // End of the anchor click event listener
+})(); // End of IIFE
